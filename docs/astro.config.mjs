5df747011357375639be97ef6a5a6d9b5b6fa90b
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2024-04-22 03:57:37 -0300

    test deploy

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://gabInteli.github.io/M10-Inteli-Eng-Comp_Gabriela_Matias/',
   	base: '/',
-  
 	integrations: [
 		starlight({
 			title: 'M10 - Gabriela',
